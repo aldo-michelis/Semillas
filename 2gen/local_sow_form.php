2e2d3418cbb80d0ab135acc6f109098e64d35293
jorajipo <jorajipo@gmail.com>
2018-07-27 14:36:46 -0500

    Campos actualizados en formulario

--- 2gen/local_sow_form.php
+++ 2gen/local_sow_form.php
@@ -3,78 +3,161 @@
 <head>
 	<title>Generador de Semillas Locales</title>
 	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<!-- Bootstrap core CSS -->
+	<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+
+	<script>
+	$( function() {
+		$( "#publication_start" ).datepicker({
+		dateFormat: 'yy-mm-dd',
+		changeYear: true,
+		yearRange: "-100:+0"
+		});
+	} );
+	</script>
+
+	<script>
+	$( function() {
+		$( "#publication_end" ).datepicker({
+		dateFormat: 'yy-mm-dd',
+		changeYear: true,
+		yearRange: "-100:+0"
+		});
+	} );
+	</script>
+
+	<script>
+	$( function() {
+		$( "#valid_start" ).datepicker({
+		dateFormat: 'yy-mm-dd',
+		changeYear: true,
+		yearRange: "-100:+0"
+		});
+	} );
+	</script>
+
+	<script>
+	$( function() {
+		$( "#valid_end" ).datepicker({
+		dateFormat: 'yy-mm-dd',
+		changeYear: true,
+		yearRange: "-100:+0"
+		});
+	} );
+	</script>
+
+
 </head>
 
-<style>
-
-  h1{text-align:center;
-  }
-  
-  table{
-    with:25%;
-    background-colour:#FFC;
-    border: 2px dotted #F00;
-    margin:auto;}
-    
-    .izq{text-align:left;
-    }
-    
-    .der{text-align:right;
-    }
-    
-    td{
-      padding:10px;
-    }
-    
-</style>
 
 <body>
-<h1> Generador de Semillas Locales</h1>
+	<div class="container">
+<h1 class="form-signin-heading"> Generador de Semillas</h1>
 
-<form name="local_sow_form" method="get" action="local_sow_insert.php">
+<form name="local_sow_form" method="get" action="local_sow_insert.php" class="form-signin">
 
- <table> 
- 
-    <tr><td>Tipo de C-milla: </td><td><input type="text" name="w_type"></td></tr>
-    <tr><td>Valor monetario de la C-milla: </td><td><input type="text" name="w_monetary_value"></td></tr>
-    <tr><td>Porcentaje de descuento: </td><td><input type="text" name="w_percent_value"></td></tr>
-    <tr><td>Descripción de la promoción: </td><td><input type="text" name="w_description"></td></tr>
-    <tr><td>Cantidad de C-millas: </td><td><input type="text" name="w_quantity_start"></td></tr>
-    <tr><td>Publicar desde: </td><td><input type="text" name="w_publication_start"></td></tr>
-    <tr><td>Publicar hasta: </td><td><input type="text" name="w_publication_end"></td></tr>
-    <tr><td>Vigencia desde: </td><td><input type="text" name="w_valid_start"></td></tr>
-    <tr><td>Vigencia hasta: </td><td><input type="text" name="w_valid_end"></td></tr>
 
-    <tr><td><input type="submit" value="Submit"></td></tr>
+	<div class="form-group row">
+		<div class="col-3">
+		<label for=w_type>Tipo de C-milla: </label>
+	</div>
+		<div class="col-9">
+			<select name="w_type" id="type"  class="form-control">
+				<option selected>Local</option>
+				<option>Universal</option>
+			</select>
+		</div>
+	</div>
+	<div class="form-group row">
+		<div class="col-3">
+		<label for="w_monetary_value">Valor monetario de la C-milla:
+		</label>
+	 </div>
+	 <div class="col-9">
+			<input type="number" name="w_monetary_value" class="form-control">
+		</div>
+	</div>
+
+	<div class="form-group row">
+		<div class="col-3">
+		<label for="w_percent_value">Porcentaje de descuento: </label>
+	</div>
+		<div class="col-9">
+		<input type="number" name="w_percent_value">
+	</div>
+</div>
+
+<div class="form-group row">
+	<div class="col-3">
+		<label for="w_description">Descripción de la promoción: </label>
+	</div>
+	<div class="col-9">
+		<input type="text" name="w_description">
+	</div>
+</div>
+
+<div class="form-group row">
+	<div class="col-3">
+		<label for="w_quantity_start">Cantidad de C-millas: </label>
+	</div>
+	<div class="col-9">
+		<input type="number" name="w_quantity_start">
+	</div>
+</div>
+
+<div class="form-group row">
+	<div class="col-3">
+	<label for"w_publication_start" >Publicar desde: </label>
+</div>
+	<div class="col-9">
+		<input type="text" name="w_publication_start" id="publication_start" class="form-control" placeholder="Selecciona..." required>
+	</div>
+</div>
+
+<div class="form-group row">
+	<div class="col-3">
+	<label for"w_publication_end" >Publicar hasta: </label>
+</div>
+	<div class="col-9">
+		<input type="text" name="w_publication_end" id="publication_end" class="form-control" placeholder="Selecciona..." required>
+	</div>
+</div>
+
+<div class="form-group row">
+	<div class="col-3">
+	<label for"w_valid_start" >Publicar desde: </label>
+</div>
+	<div class="col-9">
+		<input type="text" name="w_valid_start" id="valid_start" class="form-control" placeholder="Selecciona..." required>
+	</div>
+</div>
+
+<div class="form-group row">
+	<div class="col-3">
+	<label for"w_valid_end" >Publicar hasta: </label>
+</div>
+	<div class="col-9">
+		<input type="text" name="w_valid_end" id="valid_end" class="form-control" placeholder="Selecciona..." required>
+	</div>
+</div>
+
+
+<div class="form-group row">
+	<div class="col-3">
+		<button type="submit" class="btn btn-primary">Crear Semillas</button>
+	</div>
+</div>
 
   </table>
 
 </form>
-
+</div>
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
